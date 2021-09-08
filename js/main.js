78de34605b6325c91598e9015d64a4f585317ad6
navilur <56089501+navilur@users.noreply.github.com>
2021-09-08 23:28:05 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+// Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+// Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+
+  // Popup In
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(175px) rotateZ(-45deg)";
+  purchase.style.transform = "translateZ(75px)";
+  sizes.style.transform = "translateZ(100px)";
+  description.style.transform = "translateZ(125px)";
+});
+
+// Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+  // Popup Out
+  title.style.transform = "translateZ( 0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  purchase.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  description.style.transform = "translateZ(0px)";
+});
